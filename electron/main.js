ffec5eb7bf412d51cc0572a869d74841ee3386c8
Matthew Simon <elvatolighting@gmail.com>
2024-03-21 14:38:41 -0300

    Project Initialization

---
+++ electron/main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, '../next/out/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+
+  mainWindow.loadURL(startUrl)
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
