6058653bee5e2c61c50674666ae0a29fd31323d7
Matthew C. Simon <127530566+acdc-digital@users.noreply.github.com>
2024-03-26 12:28:42 -0300

    Cleaned Files and add Auth Routes to Dashboard

--- next/src/app/dashboard/DashboardLayout.tsx
+++ next/src/app/dashboard/DashboardLayout.tsx
@@ -1,6 +1,33 @@
-import Dashboardheader from "./_components/Dashboardheader";
+// Dashboard Layout
+// /Users/matthewsimon/Documents/GitHub/acdc.solomon-electron/solomon-electron/next/src/app/dashboard/DashboardLayout.tsx
 
-const DashboardLayout: React.FC = ({ children }) => {
+'use client'
+
+import { useConvexAuth } from 'convex/react';
+import { redirect } from 'next/navigation';
+import React from 'react';
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div>
+        <p className='text-sm'>
+           Loading...
+        </p>
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
   return (
     <div>
       <div className="dashboard-content">
