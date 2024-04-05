37307a5cbe5a26ce35db7c9ce5e787d7851c4f7a
Matthew Simon <matthewsimon@Matthews-MacBook-Pro.local>
2024-04-05 10:52:33 -0300

    Updated Title-Change  Function

--- next/convex/projects.ts
+++ next/convex/projects.ts
@@ -271,3 +271,40 @@ export const getById = query({
 	  return project;
 	}
   });
+
+  export const update = mutation({
+	args: {
+	  id: v.id("projects"),
+	  title: v.optional(v.string()),
+	  content: v.optional(v.string()),
+	  icon: v.optional(v.string()),
+	  isPublished: v.optional(v.boolean())
+	},
+	handler: async (ctx, args) => {
+	  const identity = await ctx.auth.getUserIdentity();
+  
+	  if (!identity) {
+		throw new Error("Unauthenticated");
+	  }
+  
+	  const userId = identity.subject;
+  
+	  const { id, ...rest } = args;
+  
+	  const existingProject = await ctx.db.get(args.id);
+  
+	  if (!existingProject) {
+		throw new Error("Not found");
+	  }
+  
+	  if (existingProject.userId !== userId) {
+		throw new Error("Unauthorized");
+	  }
+  
+	  const project = await ctx.db.patch(args.id, {
+		...rest,
+	  });
+  
+	  return project;
+	},
+  });
